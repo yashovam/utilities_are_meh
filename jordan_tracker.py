20d3342863a06f4042f754fdb59dae5bb56fba3c
yashovam <128621768+yashovam@users.noreply.github.com>
2026-03-01 11:25:09 -0700

    jtrak
    jordan tracker resources

---
+++ jordan_tracker.py
@@ -0,0 +1,28 @@
+
+'''
+# Webhooks Discord Server 
+jordans_channel_webhook = 'https://discord.com/api/webhooks/1189636969161040073/FagVl-yrnHjKml13RrEq_xcsXrvhNnrpbG4AfbSLDQu51Mf3RnxwKfgNRiVDEXGzaRfS'
+nike_channel_webhook = 'https://discord.com/api/webhooks/1189942534517051512/iGUWnhTRQRNZSleCldyubvn-tFxCEGOwACBYmS1_R8OUf3B8qeqXUiRE8gVzsbt--Lgs'
+snkrs_channel_webhook = 'https://discord.com/api/webhooks/1191019888966381698/n7wO50NhtL170-IzY-RgxQwrpHJN0fXhwp8pzLR5YWh2-0Iye8msspecVQAsygVRQ4DR'
+testing_webhook = 'https://discord.com/api/webhooks/1189962255274610720/69gMnTcyLojjSNDw1PsYbK4-_j4eYUo3EewkykbCOqLWBUjfnZAUxHFys0RXqJmnQWnY'
+
+# The APIs from which the data is being fetched
+air_jordans_api_target_url = 'https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=5BFC52F66E37C95FCB641DBC401EB101&country=gb&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(GB)%26filter%3Dlanguage(en-GB)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c%2C193af413-39b0-4d7e-ae34-558821381d3f%2C498ac76f-4c2c-4b55-bbdc-dd37011887b1)%26anchor%3D24%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=en-GB&localizedRangeStr=%7BlowestPrice%7D%E2%80%94%7BhighestPrice%7D'
+nike_api_target_url = 'https://api.nike.com/cic/browse/v2?queryid=products&anonymousId=5BFC52F66E37C95FCB641DBC401EB101&country=gb&endpoint=%2Fproduct_feed%2Frollup_threads%2Fv2%3Ffilter%3Dmarketplace(GB)%26filter%3Dlanguage(en-GB)%26filter%3DemployeePrice(true)%26filter%3DattributeIds(0f64ecc7-d624-4e91-b171-b83a03dd8550%2C16633190-45e5-4830-a068-232ac7aea82c%2C193af413-39b0-4d7e-ae34-558821381d3f)%26anchor%3D24%26consumerChannelId%3Dd9a5bc42-4b9c-4976-858a-f159cf99c647%26count%3D24&language=en-GB&localizedRangeStr=%7BlowestPrice%7D%E2%80%94%7BhighestPrice%7D'
+snkrs_api_target_url = 'https://api.nike.com/product_feed/threads/v3/?anchor=50&count=50&filter=marketplace%28GB%29&filter=language%28en-GB%29&filter=inStock%28true%29&filter=productInfo.merchPrice.discounted%28false%29&filter=channelId%28010794e5-35fe-4e32-aaff-cd2c74f89d61%29&filter=exclusiveAccess%28true%2Cfalse%29'
+
+'''
+
+
+
+
+url_list = [
+    'https://2fwotdvm2o-3.algolianet.com/1/indexes/*/queries',
+    'https://www.goat.com/web-api/v1/product_templates/',
+    'https://www.goat.com/sneakers/',
+    'https://api.thesneakerdatabase.com/v1/sneakers?limit=100&brand=adidas',
+    'https://www.nike.com/in/t/air-max-sc-shoes-gMGhP8/FJ3242-100',
+    'https://www.nike.com/in/t/air-force-1-07-shoes-9nHjKZ/315122-111',####?????????
+    'https://www.nike.com/in/t/air-jordan-1-mid-shoes-9nHjKZ/554724-074',###?
+    'https://www.nike.com/gb/t/dunk-low-retro-shoes-szNRv1/FB3354-001'
+]
